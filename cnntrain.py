f4f240f1fde66c4728b6f9feccf8bc9e7644cf03
domipm <dpas602@gmail.com>
2024-11-15 01:42:11 +0100

    Added train loop

--- cnntrain.py
+++ cnntrain.py
@@ -6,19 +6,22 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.functional as F
 
 from torch.utils.data import DataLoader
-
-import dataloader
-import cnnmodel
+from torchvision.utils import make_grid
 
 from datetime import datetime
 
+import dataloader
+import cnnmodel
+        
 # Folder paths for testing and training images
 train_dir = "./catdog_data/train/"
 test_dir = "./catdog_data/test/"
 valid_dir = "./catdog_data/validation/"
+
+# Path where to save model parameters
+save_path = "./model_params.pt"
 
 # Seed for random numbers (based on current time, change to set fixed seed)
 rand_seed = datetime.now().timestamp()
@@ -27,18 +30,69 @@ random.seed(rand_seed)
 # Set seed for pytorch
 torch.manual_seed(rand_seed)
 
-# PyTorch DataLoader parameters
+# Batchsize to load from data
 batchsize = 32
-workers = 0
 
 # Initialize training dataset
 train_dataset = dataloader.CatsDogsDataset(train_dir)
-test_dataset = dataloader.CatsDogsDataset(test_dir)
 # Load dataset into pytorch
-train_loader = DataLoader(train_dataset, batch_size = batchsize, num_workers = workers, shuffle = True)
-test_loader = DataLoader(test_dataset, batch_size = batchsize, num_workers = workers, shuffle = False)
+train_loader = DataLoader(train_dataset, batch_size = batchsize, shuffle = True)
+
+# Function to show images of batch
+def show_batch(dataloader, show):
+    for img, _ in dataloader:
+        _,ax = plt.subplots()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.imshow(make_grid(img,nrow=4).permute(1,2,0))
+        break
+    if show == True: plt.show()
+# Show images in a batch
+show_batch(train_loader, show = False)
 
 # Initiaize CNN model
 model = cnnmodel.CNN()
 # Print-out model's summary
 model.model_summary(in_size = (tuple(train_dataset[0][0].shape)))
+
+# Training Hyperparameters
+epochs = 1
+learning_rate = 0.001
+
+# Loss Function
+criterion = nn.NLLLoss()
+# Optimizer (Stochastic Gradient Descent)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+def train(dataloader, model, criterion, optimizer):
+
+    # Set model to train mode
+    model.train()
+
+    # Run over all batches in dataloader, and enumerate for images and labels
+    for batch, (X, label) in enumerate(dataloader):
+
+        # Reset gradient to zero for every batch
+        optimizer.zero_grad()
+        # Compute prediction
+        pred = model(X)
+        # Compute loss between prediction and real label
+        loss = criterion(pred, label)
+        # Backpropagation to compute gradients
+        loss.backward()
+        # Perform one step of optimizer, adjusting weights
+        optimizer.step()
+
+        # Debug parameter gradients
+        #for name, param in model.named_parameters():
+        #    print(name, param.grad)
+
+        print('Batch {}/{}\tLoss: {:f}'.format(batch+1, len(dataloader), loss.item()))
+
+for epoch in range(epochs):
+
+    print(f"\nEpoch {epoch+1}\n" + "-"*64)
+    train(train_loader, model, criterion, optimizer)
+
+# After training the model, save the parameters
+torch.save(model.state_dict(), save_path)
