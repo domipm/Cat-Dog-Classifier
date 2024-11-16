22bee8811d8e2c1d428f37ec01775f4f1e0f05af
domipm <dpas602@gmail.com>
2024-11-16 23:23:07 +0100

    CNN Model architecture update, code cleanup

--- cnnmodel.py
+++ cnnmodel.py
@@ -5,68 +5,36 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torchsummary import summary
-
 class CNN(nn.Module):
 
     # Initialization function with definitions for all layers
-    def __init__(self):
+    def __init__(self, in_shape = None):
 
         super().__init__()
 
-        # Convolutional Layers
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        # Define all layer to be used
+        self.network = nn.Sequential(
+                       nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),   # Convolution Layer
+                       nn.BatchNorm2d(num_features=16),                                                 # Batch Normalization
+                       nn.ReLU(),                                                                       # ReLU Activation
+                       nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                                # Max Pooling Layer
+                       nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1),  # Convolution Layer
+                       nn.BatchNorm2d(num_features=64),                                                 # Batch Normalization
+                       nn.ReLU(),                                                                       # ReLU Activation
+                       nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                                # Max Pooling Layer 
+                       nn.Flatten(start_dim=1),                                                         # Flatten Spatial Dimensions
+                       nn.LazyLinear(out_features=16),                                                  # Dense Layer
+                       nn.ReLU(),                                                                       # ReLU Activation
+                       nn.Dropout(p=0.25),                                                              # Dropout Layer
+                       nn.LazyLinear(out_features=2)                                                    # Dense Layer
+        )
 
-        # Pooling Layers
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-
-        # Fully-Connected Dense Layers
-        self.dense1 = nn.LazyLinear(out_features=128)
-        self.dense2 = nn.LazyLinear(out_features=2)
+        if in_shape != None:
+            summary(self, in_shape)
 
         return
     
     # Forward pass of the networks
     def forward(self, x):
 
-        # First Convolutional Layer + ReLU Activation
-        # (3x512x512) -> (16x512x512)
-        x = self.conv1(x)
-        x = F.relu(x)
-
-        # First Pooling Layer
-        # (16x512x512) -> (16x256x256)
-        x = self.pool(x)
-
-        # Second Convolutional Layer + ReLU Activation
-        # (16x256x256) -> (32x256x256)
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        # Second Pooling Layer
-        # (32x256x256) -> (32x128x128)
-        x = self.pool(x)
-
-        # Flatten Tensor to Vector
-        # (32x128x128) -> [32x128x128]
-        x = x.view(-1, 32*128*128)
-
-        # First Fully-Connected (Dense) Layer
-        # [32x128x128] -> [128]
-        x = self.dense1(x)
-        x = F.relu(x)
-
-        # Second Fully-Connected (Dense) Layer
-        # [128] -> [2]
-        x = self.dense2(x)
-
-        # Non-Linear Activation Function (on spatial dimension)
-        output = F.log_softmax(x, dim=1)
-        
-        return output
-    
-    # Function to print out model's summary
-    def model_summary(self, in_size):
-
-        summary(self, input_size = in_size)
-        return
+        return self.network(x) # Return output from last dense layer (without any activation function, 'raw' logits)
