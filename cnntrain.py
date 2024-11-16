5b52e4d4053dd5a760d51e8e885bad610a5222df
domipm <dpas602@gmail.com>
2024-11-16 23:23:29 +0100

    Training and testing fixed

--- cnntrain.py
+++ cnntrain.py
@@ -1,4 +1,3 @@
-import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,16 +8,18 @@ import torch.optim as optim
 
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
+from torchvision.transforms import v2
 
 from datetime import datetime
 
+from alive_progress import alive_bar
+
 import dataloader
 import cnnmodel
-        
+
 # Folder paths for testing and training images
 train_dir = "./catdog_data/train/"
 test_dir = "./catdog_data/test/"
-valid_dir = "./catdog_data/validation/"
 
 # Path where to save model parameters
 save_path = "./model_params.pt"
@@ -30,33 +31,33 @@ random.seed(rand_seed)
 # Set seed for pytorch
 torch.manual_seed(rand_seed)
 
+# Transform parameters
+image_size = (64,)*2
+
+# Define transform to perform on images (augmentation)
+image_transform = v2.Compose([
+                    v2.ToImage(),                               # Convert to image object
+                    v2.ToDtype(torch.float32, scale=True),      # Convert to tensor
+                    v2.Resize(image_size),                      # Resize images to same size
+                    v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))   # Normalize images
+                ])
+
+# Initialize datasets
+train_dataset = dataloader.CatsDogsDataset(train_dir, image_transform)
+test_dataset = dataloader.CatsDogsDataset(test_dir, image_transform)
+
 # Batchsize to load from data
 batchsize = 16
 
-# Initialize training dataset
-train_dataset = dataloader.CatsDogsDataset(train_dir)
-# Load dataset into pytorch
+# Load datasets into pytorch
 train_loader = DataLoader(train_dataset, batch_size = batchsize, shuffle = True)
-
-# Function to show images of batch
-def show_batch(dataloader, show):
-    for img, _ in dataloader:
-        _,ax = plt.subplots()
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.imshow(make_grid(img,nrow=4).permute(1,2,0))
-        break
-    if show == True: plt.show()
-# Show images in a batch
-show_batch(train_loader, show = False)
+test_loader = DataLoader(test_dataset, batch_size = batchsize, shuffle = True)
 
 # Initiaize CNN model
-model = cnnmodel.CNN()
-# Print-out model's summary
-model.model_summary(in_size = (tuple(train_dataset[0][0].shape)))
+model = cnnmodel.CNN(train_dataset[0][0].shape)
 
 # Training Hyperparameters
-epochs = 5
+epochs = 10
 learning_rate = 0.001
 
 # Loss Function
@@ -64,31 +65,81 @@ criterion = nn.CrossEntropyLoss()
 # Optimizer (Stochastic Gradient Descent)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-def train(dataloader, model, criterion, optimizer):
+# Loss Function
+criterion = nn.CrossEntropyLoss()
+# Optimizer
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Arrays for storing loss evolution over epochs
+train_loss_epoch = []
+test_loss_epoch = []
+
+# Run over multiple epochs
+for epoch in range(epochs):
+
+    # Reset train and test losses
+    train_loss = 0
+    test_loss = 0
 
     # Set model to train mode
     model.train()
 
-    # Run over all batches in dataloader, and enumerate for images and labels
-    for batch, (X, label) in enumerate(dataloader):
+    # Bar for training progress visualization
+    with alive_bar(total=len(train_loader),
+                   max_cols=64,
+                   title="Epoch {}".format(epoch+1),
+                   bar="classic", 
+                   spinner=None, 
+                   monitor="Batch {count}/{total}", 
+                   elapsed="[{elapsed}]",
+                   elapsed_end="[{elapsed}]",
+                   stats=None) as bar:
+        
+        # Run over all batches
+        for batch, (image, label) in enumerate(train_loader):
 
-        # Reset gradient to zero for every batch
-        optimizer.zero_grad()
-        # Compute prediction
-        pred = model(X)
-        # Compute loss between prediction and real label
-        loss = criterion(input=pred, target=label)
-        # Backpropagation to compute gradients
-        loss.backward()
-        # Perform one step of optimizer, adjusting weights
-        optimizer.step()
-        # Print progress parameters
-        print('Batch {}/{}\tLoss: {:f}'.format(batch+1, len(dataloader), loss.item()))
+            # Reset gradients
+            optimizer.zero_grad()
+            # Output from model
+            output = model(image)
+            # Compute loss
+            loss = criterion(output, label)
+            # Backpropagation
+            loss.backward()
+            # Perform one step of optimizer
+            optimizer.step()
 
-# Run over all epochs
-for epoch in range(epochs):
-    print(f"\nEpoch {epoch+1}\n" + "-"*64)
-    train(train_loader, model, criterion, optimizer)
+            # Add current batch loss to train loss
+            train_loss += loss.item()
+
+            # Update progress bar
+            bar()
+
+    # Compute average training loss for current epoch
+    avg_train_loss = train_loss / len(train_loader)
+    train_loss_epoch.append(avg_train_loss)  
+    # Print out average training loss for each epoch 
+    print('- Avg. Epoch Train Loss: {:.6f}'.format(avg_train_loss)) 
+
+    # Set model to eval mode
+    model.eval()
+
+    # Ensure no gradient is computed
+    with torch.no_grad():
+
+        for batch, (image, label) in enumerate(test_loader):
+            # Output from model
+            output = model(image)
+            # Compute loss
+            loss = criterion(output, label)
+            # Add current batch loss to test loss
+            test_loss += loss.item()
+
+    # Compute average test loss for current epoch
+    avg_test_loss = test_loss / len(test_loader)
+    test_loss_epoch.append(avg_test_loss) 
+    # Print out average training loss for each epoch 
+    print('- Avg. Epoch Test  Loss: {:.6f}'.format(avg_test_loss), end="\n\n") 
 
 # After training the model, save the parameters
 torch.save(model.state_dict(), save_path)
