f236dbbd4ee9ab4c9cff03dda2c51377b1037a43
domipm <dpas602@gmail.com>
2024-11-17 12:16:15 +0100

    Included test and train accuracy and loss logs

--- cnntrain.py
+++ cnntrain.py
@@ -22,8 +22,8 @@ import cnnmodel
 train_dir = "./catdog_data/train/"
 test_dir = "./catdog_data/test/"
 
-# Path where to save model parameters
-save_path = "./model_params.pt"
+# Directory for writing all the outputs, graphs, and model parameters
+output_dir = "./output/"
 
 # Seed for random numbers (based on current time, change to set fixed seed)
 rand_seed = datetime.now().timestamp()
@@ -33,22 +33,41 @@ random.seed(rand_seed)
 torch.manual_seed(rand_seed)
 
 # Transform parameters
-image_size = (64,)*2
+image_size = (32,)*2
+p_hflip = 0.5
+p_grayscale = 0.25
+p_invert = 0.15
+degrees = 35
 
-# Define transform to perform on images (augmentation)
-image_transform = v2.Compose([
+# Define transform to perform on training images (augmentation)
+train_transform = v2.Compose([
                     v2.ToImage(),                               # Convert to image object
                     v2.ToDtype(torch.float32, scale=True),      # Convert to tensor
                     v2.Resize(image_size),                      # Resize images to same size
-                    v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))   # Normalize images
+                    #v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),  # Normalize images
+                    v2.RandomHorizontalFlip(p_hflip),
+                    #v2.GaussianNoise(mean=np.random.uniform(0,0.15),
+                    #                 sigma=np.random.uniform(0,0.25)),
+                    #v2.ColorJitter(),
+                    v2.RandomRotation(degrees),
+                    #v2.RandomGrayscale(p_grayscale),
+                    #v2.RandomAdjustSharpness(sharpness_factor=np.random.uniform(0,2)),
+                    #v2.RandomInvert(p_invert)
                 ])
 
+# Define transform to perform on testing images (just transform to tensor)
+test_transform = v2.Compose([
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=True),
+                    v2.Resize(image_size),
+])
+
 # Initialize datasets
-train_dataset = dataloader.CatsDogsDataset(train_dir, image_transform)
-test_dataset = dataloader.CatsDogsDataset(test_dir, image_transform)
+train_dataset = dataloader.CatsDogsDataset(train_dir, train_transform)
+test_dataset = dataloader.CatsDogsDataset(test_dir, test_transform)
 
 # Batchsize to load from data
-batchsize = 16
+batchsize = 32
 
 # Load datasets into pytorch
 train_loader = DataLoader(train_dataset, batch_size = batchsize, shuffle = True)
@@ -58,13 +77,8 @@ test_loader = DataLoader(test_dataset, batch_size = batchsize, shuffle = True)
 model = cnnmodel.CNN(train_dataset[0][0].shape)
 
 # Training Hyperparameters
-epochs = 5
-learning_rate = 0.001
-
-# Loss Function
-criterion = nn.CrossEntropyLoss()
-# Optimizer (Stochastic Gradient Descent)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+epochs = 3
+learning_rate = 0.00075
 
 # Loss Function
 criterion = nn.CrossEntropyLoss()
@@ -74,6 +88,9 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 # Arrays for storing loss evolution over epochs
 train_loss_epoch = []
 test_loss_epoch = []
+# Arrays for storing accuracy over epochs
+train_accuracy_epoch = []
+test_accuracy_epoch = []
 
 # Run over multiple epochs
 for epoch in range(epochs):
@@ -81,6 +98,9 @@ for epoch in range(epochs):
     # Reset train and test losses
     train_loss = 0
     test_loss = 0
+    # Reset train and test accuracy
+    train_accuracy = 0
+    test_accuracy = 0
 
     # Set model to train mode
     model.train()
@@ -98,7 +118,6 @@ for epoch in range(epochs):
         
         # Run over all batches
         for batch, (image, label) in enumerate(train_loader):
-
             # Reset gradients
             optimizer.zero_grad()
             # Output from model
@@ -109,18 +128,21 @@ for epoch in range(epochs):
             loss.backward()
             # Perform one step of optimizer
             optimizer.step()
-
             # Add current batch loss to train loss
             train_loss += loss.item()
-
+            # Add current batch accuracy to train accuracy
+            train_accuracy += (output.argmax(1) == label).float().sum()
             # Update progress bar
             bar()
 
     # Compute average training loss for current epoch
     avg_train_loss = train_loss / len(train_loader)
     train_loss_epoch.append(avg_train_loss)  
+    # Compute averate training accuracy for current epoch
+    avg_train_accuracy = 100 * train_accuracy / len(train_dataset)
+    train_accuracy_epoch.append(avg_train_accuracy)
     # Print out average training loss for each epoch 
-    print('- Avg. Epoch Train Loss: {:.6f}'.format(avg_train_loss)) 
+    print('- Avg. Train Loss: {:.6f}\t Avg. Train Accuracy {:.6f}'.format(avg_train_loss, avg_train_accuracy)) 
 
     # Set model to eval mode
     model.eval()
@@ -135,20 +157,29 @@ for epoch in range(epochs):
             loss = criterion(output, label)
             # Add current batch loss to test loss
             test_loss += loss.item()
+            # Add current batch accuracy to test accuracy
+            test_accuracy += (output.argmax(1) == label).float().sum()
 
     # Compute average test loss for current epoch
     avg_test_loss = test_loss / len(test_loader)
     test_loss_epoch.append(avg_test_loss) 
-    # Print out average training loss for each epoch 
-    print('- Avg. Epoch Test  Loss: {:.6f}'.format(avg_test_loss), end="\n\n") 
+    # Compute averate training accuracy for current epoch
+    avg_test_accuracy = 100 * test_accuracy / len(test_dataset)
+    test_accuracy_epoch.append(avg_test_accuracy)
+    # Print out average testing loss for each epoch 
+    print('- Avg. Test Loss: {:.6f}\t Avg. Test Accuracy {:.6f}'.format(avg_test_loss, avg_test_accuracy), end="\n\n") 
 
 print("-"*64)
 
 # After training the model, save the parameters
-torch.save(model.state_dict(), save_path)
+torch.save(model.state_dict(), output_dir + "model_params.pt")
 
-# Directory to save plot
-plot_dir = "./graphs/"
+# Write the training and testing loss and accuracy to file !
+with open(output_dir + "output_logs.txt", "w") as f:
+    for epoch in range(epochs):
+        f.write("{}\t{}\t{}\t{}\t{}\n".format(epochs, train_loss_epoch[epoch], test_loss_epoch[epoch], train_accuracy_epoch[epoch], test_accuracy_epoch[epoch]))
+f.close()
+
 # Plot training and testing loss over time
 fig, ax = plt.subplots()
 ax.plot(np.arange(epochs), train_loss_epoch, label="Train loss")
@@ -158,5 +189,19 @@ ax.set_xlabel("Epochs")
 ax.set_ylabel("Average epoch loss")
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 plt.legend()
-plt.savefig(plot_dir + str("loss_traintest.png"), dpi=300)
+plt.savefig(output_dir + "loss_testtrain.png", dpi=300)
 plt.show()
+plt.close()
+
+# Plot training and testing accuracy over time
+fig, ax = plt.subplots()
+ax.plot(np.arange(epochs), train_accuracy_epoch, label="Train accuracy")
+ax.plot(np.arange(epochs), test_accuracy_epoch, label="Test accuracy")
+ax.set_title("Train/Test Accuracy")
+ax.set_xlabel("Epochs")
+ax.set_ylabel("Average epoch accuracy [%]")
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.legend()
+plt.savefig(output_dir + "accuracy_testtrain.png", dpi=300)
+plt.show()
+plt.close()
