33446eafc0399785cad130e0b70cf77545643390
domipm <dpas602@gmail.com>
2024-11-19 06:51:59 +0100

    Sample images (old code) moved to folder

--- samples/sampleimages.py
+++ samples/sampleimages.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import torch
 
 from torch.utils.data import DataLoader
+
 from torchvision.utils import make_grid
 from torchvision.transforms import v2
 
@@ -27,7 +28,7 @@ random.seed(rand_seed)
 torch.manual_seed(rand_seed)
 
 # Transform parameters
-image_size = (64,)*2
+image_size = (128,)*2
 p_hflip = 0.5
 p_grayscale = 0.25
 p_invert = 0.15
@@ -36,28 +37,29 @@ degrees = 35
 # Define transform to perform on images (augmentation)
 image_transform = v2.Compose([
                     v2.ToImage(),                               # Convert to image object
-                    v2.ToDtype(torch.float32, scale=True),      # Convert to tensor
-                    v2.Resize(image_size),                      # Resize images to same size
-                    v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),  # Normalize images
-                    v2.RandomHorizontalFlip(p_hflip),
-                    v2.GaussianNoise(mean=np.random.uniform(0,0.15),
-                                     sigma=np.random.uniform(0,0.25)),
-                    v2.ColorJitter(),
-                    v2.RandomRotation(degrees),
-                    v2.RandomGrayscale(p_grayscale),
-                    v2.RandomAdjustSharpness(sharpness_factor=np.random.uniform(0,2)),
-                    v2.RandomInvert(p_invert)
+                    #v2.ToDtype(torch.float32, scale=True),      # Convert to tensor
+                    #v2.Resize(image_size),                      # Resize images to same size
+                    #v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),  # Normalize images
+                    #v2.RandomHorizontalFlip(p_hflip),
+                    #v2.GaussianNoise(mean=np.random.uniform(0,0.05),
+                    #                 sigma=np.random.uniform(0,0.05)),
+                    #v2.ColorJitter(),
+                    #v2.RandomRotation(degrees),
+                    #v2.RandomGrayscale(p_grayscale),
+                    #v2.RandomAdjustSharpness(sharpness_factor=np.random.uniform(0,2)),
+                    #v2.RandomInvert(p_invert),
+                    v2.RandAugment(num_ops=3, magnitude=5)
                 ])
 
 # Initialize datasets
 dataset = dataloader.CatsDogsDataset(data_dir, image_transform)
 
 # Show random image from dataset
-img, _, img_name = dataset.__getitem__(np.random.randint(len(dataset)), transformed=False, return_name=True)
+img, _, img_name = dataset.__getitem__(np.random.randint(len(dataset)), transformed=False, return_name=True, normalize=False)
 plt.imshow(img.permute(1,2,0))
 plt.axis("Off")
 plt.title(img_name)
-plt.savefig(save_path + "sample_image.png", dpi=300, bbox_inches="tight")
+#plt.savefig(save_path + "sample_image.png", dpi=300, bbox_inches="tight")
 plt.show()
 plt.close()
 
@@ -77,11 +79,9 @@ for n in range(n_images):
     ax[n+1].imshow(img_aug.permute(1,2,0))
     ax[n+1].axis("Off")
     ax[n+1].set_title(img_name + str(" (Aug)"))
-plt.savefig(save_path + "sample_image_transformed.png", dpi=300, bbox_inches="tight")
+#plt.savefig(save_path + "sample_image_transformed.png", dpi=300, bbox_inches="tight")
 plt.show()
 plt.close()
-
-exit()
 
 # Batchsize to load from data
 batchsize = 16
@@ -94,8 +94,8 @@ for img, _ in loader:
     _, ax = plt.subplots()
     ax.set_title("Sample batch")
     ax.axis("Off")
-    ax.imshow(make_grid(img, nrow=4).permute(1,2,0))
+    ax.imshow(make_grid(img, nrow=4).permute(1,2,0), vmin=0, vmax=1)
     break
-plt.savefig(save_path + "sample_batch.png", dpi=300, bbox_inches="tight")
+#plt.savefig(save_path + "sample_batch.png", dpi=300, bbox_inches="tight")
 plt.show()
 plt.close()
